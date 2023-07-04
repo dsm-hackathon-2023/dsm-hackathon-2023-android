2545e921e7e03ed4d45e0ec28eeb0d99ec52701f
Junsu <krpjs0508@gmail.com>
2023-07-04 21:08:33 +0900

    bg color

--- app/src/main/kotlin/dsm/hackathon/dsmhackathon2023_team18/ui/main/home/calendar/CalendarScreen.kt
+++ app/src/main/kotlin/dsm/hackathon/dsmhackathon2023_team18/ui/main/home/calendar/CalendarScreen.kt
@@ -8,8 +8,8 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
-import dsm.hackathon.dsmhackathon2023_team18.ui.theme.Background
 import dsm.hackathon.dsmhackathon2023_team18.ui.theme.Primary
 
 @Composable
@@ -26,7 +26,7 @@ fun CalendarScreen(
             modifier = modifier
                 .fillMaxSize()
                 .background(
-                    color = Background,
+                    color = Color.White,
                     shape = RoundedCornerShape(
                         topStart = 32.dp,
                         topEnd = 32.dp,
