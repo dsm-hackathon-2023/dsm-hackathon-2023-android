b7d52a895844287ddb279b896b0f50aa3b0850e1
junsuPark <krpjs0508@gmail.com>
2023-07-04 15:22:23 +0900

    auth nav

--- app/src/main/kotlin/dsm/hackathon/dsmhackathon2023_team18/ui/auth/AuthNavigation.kt
+++ app/src/main/kotlin/dsm/hackathon/dsmhackathon2023_team18/ui/auth/AuthNavigation.kt
@@ -1,10 +1,18 @@
 package dsm.hackathon.dsmhackathon2023_team18.ui.auth
 
 import androidx.navigation.NavGraphBuilder
+import androidx.navigation.compose.composable
 import androidx.navigation.navigation
 
 fun NavGraphBuilder.authNavigation() {
-    // navigation()
+    navigation(
+        route = AuthDestination.route,
+        startDestination = AuthDestination.login,
+    ) {
+        composable(AuthDestination.login) {
+
+        }
+    }
 }
 
 object AuthDestination {
