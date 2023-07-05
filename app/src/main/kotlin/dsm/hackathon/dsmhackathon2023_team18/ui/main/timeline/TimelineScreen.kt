b375c52bc2ac3b67caf09e2c5d6d7b15ff0aabb4
Junsu <krpjs0508@gmail.com>
2023-07-05 19:47:57 +0900

    brush

--- app/src/main/kotlin/dsm/hackathon/dsmhackathon2023_team18/ui/main/timeline/TimelineScreen.kt
+++ app/src/main/kotlin/dsm/hackathon/dsmhackathon2023_team18/ui/main/timeline/TimelineScreen.kt
@@ -8,10 +8,13 @@ import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.pager.VerticalPager
 import androidx.compose.foundation.pager.rememberPagerState
@@ -21,20 +24,34 @@ import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.util.lerp
+import dsm.hackathon.dsmhackathon2023_team18.LocalPrimaryColor
 import dsm.hackathon.dsmhackathon2023_team18.R
 import dsm.hackathon.dsmhackathon2023_team18.ui.theme.Gray1
 import kotlin.math.absoluteValue
+
+val listFadeBrush: Brush
+    @Composable get() = Brush.verticalGradient(
+        colors = listOf(
+            Color.White,
+            Color.White,
+            Color.Transparent,
+        ),
+    )
 
 @OptIn(ExperimentalFoundationApi::class)
 @SuppressLint("SetJavaScriptEnabled")
@@ -72,47 +89,71 @@ fun TimelineScreen(
             },
             colors = topAppBarColors,
         )
-        Image(
-            painter = painterResource(id = R.drawable.img_timeline_date),
-            contentDescription = null,
-        )
+        Box(
+            contentAlignment = Alignment.Center,
+        ) {
+            Icon(
+                painter = painterResource(id = R.drawable.ic_timeline_date),
+                contentDescription = null,
+                tint = LocalPrimaryColor.current,
+            )
+            Text(
+                text = "6 금",
+                color = Color.White,
+                style = MaterialTheme.typography.titleMedium,
+                fontWeight = FontWeight.SemiBold,
+            )
+        }
         val pagerState = rememberPagerState { 3 }
-        VerticalPager(
+        Box(
             modifier = Modifier.fillMaxSize(),
-            state = pagerState,
-            contentPadding = PaddingValues(top = 72.dp),
-        ) { page ->
-            Image(
-                modifier = Modifier
-                    .graphicsLayer {
-                        val pagerOffset =
-                            ((pagerState.currentPage - page) + pagerState.currentPageOffsetFraction).absoluteValue
+            contentAlignment = Alignment.TopCenter,
+        ) {
+            VerticalPager(
+                modifier = Modifier.fillMaxSize(),
+                state = pagerState,
+                contentPadding = PaddingValues(top = 72.dp),
+            ) { page ->
+                Image(
+                    modifier = Modifier
+                        .graphicsLayer {
+                            val pagerOffset =
+                                ((pagerState.currentPage - page) + pagerState.currentPageOffsetFraction).absoluteValue
 
-                        lerp(
-                            start = 0.875f, stop = 1f, fraction = 1f - pagerOffset.coerceIn(0f, 1f)
-                        ).also { scale ->
-                            scaleX = scale
-                            scaleY = scale
+                            lerp(
+                                start = 0.875f,
+                                stop = 1f,
+                                fraction = 1f - pagerOffset.coerceIn(0f, 1f)
+                            ).also { scale ->
+                                scaleX = scale
+                                scaleY = scale
+                            }
+                            alpha = lerp(
+                                start = 0.5f,
+                                stop = 1f,
+                                fraction = 1f - pagerOffset.coerceIn(0f, 1f),
+                            )
                         }
-                        alpha = lerp(
-                            start = 0.5f,
-                            stop = 1f,
-                            fraction = 1f - pagerOffset.coerceIn(0f, 1f),
-                        )
-                    }
+                        .fillMaxWidth()
+                        .padding(horizontal = 16.dp)
+                        .clip(RoundedCornerShape(20.dp))
+                        .clickable(onClick = onNavigateToDetails),
+                    painter = painterResource(
+                        id = when (page) {
+                            0 -> R.drawable.dummy_img_timeline_1
+                            1 -> R.drawable.dummy_img_timeline_2
+                            2 -> R.drawable.dummy_img_timeline_3
+                            else -> throw IllegalStateException()
+                        },
+                    ),
+                    contentDescription = "list item",
+                )
+            }
+            Spacer(
+                Modifier
                     .fillMaxWidth()
-                    .padding(horizontal = 16.dp)
-                    .clip(RoundedCornerShape(20.dp))
-                    .clickable(onClick = onNavigateToDetails),
-                painter = painterResource(
-                    id = when (page) {
-                        0 -> R.drawable.dummy_img_timeline_1
-                        1 -> R.drawable.dummy_img_timeline_2
-                        2 -> R.drawable.dummy_img_timeline_3
-                        else -> throw IllegalStateException()
-                    },
-                ),
-                contentDescription = "list item",
+                    .height(36.dp)
+                    .background(listFadeBrush),
             )
         }
     }
