d212e710cfa4ef5abea183424577735246bbea7f
Junsu <krpjs0508@gmail.com>
2023-07-06 07:20:05 +0900

    img

---
+++ app/src/main/kotlin/dsm/hackathon/dsmhackathon2023_team18/util/NotificationUtils.kt
@@ -0,0 +1,41 @@
+package dsm.hackathon.dsmhackathon2023_team18.util
+
+import android.annotation.SuppressLint
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
+import dsm.hackathon.dsmhackathon2023_team18.MainActivity
+import dsm.hackathon.dsmhackathon2023_team18.R
+
+@SuppressLint("MissingPermission")
+fun Context.sendNotification(
+    title: String,
+    content: String,
+) {
+    val intent = Intent(this, MainActivity::class.java).apply {
+        flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+    }
+    val pendingIntent: PendingIntent = PendingIntent.getActivity(
+        this, 0, intent, PendingIntent.FLAG_MUTABLE,
+    )
+    val builder = NotificationCompat.Builder(this, "").setSmallIcon(R.drawable.ic_tiger)
+        .setContentTitle(title).setContentText(content)
+        .setPriority(NotificationCompat.PRIORITY_DEFAULT).setContentIntent(pendingIntent)
+    val name = "Tiger"
+    val descriptionText = "Tiger gives you some advices"
+    val importance = NotificationManager.IMPORTANCE_DEFAULT
+    val channel = NotificationChannel("ch_onui_notification", name, importance).apply {
+        description = descriptionText
+    }
+    // Register the channel with the system
+    val notificationManager: NotificationManager =
+        getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+    notificationManager.createNotificationChannel(channel)
+    with(NotificationManagerCompat.from(this)) {
+        notify(System.currentTimeMillis().hashCode(), builder.build())
+    }
+}
