0aa8eae3779da3e59b369ab2569993aa8e424ebb
Junsu <krpjs0508@gmail.com>
2023-07-05 19:42:33 +0900

    timeline

--- app/src/main/kotlin/dsm/hackathon/dsmhackathon2023_team18/ui/main/timeline/TimelineNavigation.kt
+++ app/src/main/kotlin/dsm/hackathon/dsmhackathon2023_team18/ui/main/timeline/TimelineNavigation.kt
@@ -6,6 +6,7 @@ import androidx.navigation.NavGraphBuilder
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.composable
 import androidx.navigation.navigation
+import dsm.hackathon.dsmhackathon2023_team18.ui.main.timeline.detail.TimelineDetailsScreen
 
 object TimelineNavigation {
     const val route = "timeline"
@@ -25,10 +26,14 @@ fun NavGraphBuilder.timelineNavigation(
             TimelineScreen(
                 modifier = Modifier.fillMaxSize(),
                 onNavigateUp = navController::navigateUp,
+                onNavigateToDetails = navController::navigateToTimelineDetails,
             )
         }
         composable(TimelineNavigation.details) {
-
+            TimelineDetailsScreen(
+                modifier = Modifier.fillMaxSize(),
+                onNavigateUp = navController::navigateUp,
+            )
         }
     }
 }
@@ -37,5 +42,11 @@ fun NavHostController.navigateToTimelineNav() {
     this.navigate(TimelineNavigation.route) {
         launchSingleTop = true
         restoreState = true
+    }
+}
+
+fun NavHostController.navigateToTimelineDetails() {
+    this.navigate(TimelineNavigation.details) {
+        launchSingleTop = true
     }
 }
--- app/src/main/kotlin/dsm/hackathon/dsmhackathon2023_team18/ui/main/timeline/TimelineScreen.kt
+++ app/src/main/kotlin/dsm/hackathon/dsmhackathon2023_team18/ui/main/timeline/TimelineScreen.kt
@@ -7,6 +7,7 @@ import android.webkit.JavascriptInterface
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
@@ -14,6 +15,7 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.pager.VerticalPager
 import androidx.compose.foundation.pager.rememberPagerState
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -24,12 +26,12 @@ import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.util.lerp
-import com.google.accompanist.web.rememberWebViewState
 import dsm.hackathon.dsmhackathon2023_team18.R
 import dsm.hackathon.dsmhackathon2023_team18.ui.theme.Gray1
 import kotlin.math.absoluteValue
@@ -40,9 +42,8 @@ import kotlin.math.absoluteValue
 fun TimelineScreen(
     modifier: Modifier = Modifier,
     onNavigateUp: () -> Unit,
+    onNavigateToDetails: () -> Unit,
 ) {
-    val webViewState = rememberWebViewState(url = "https://onui.vercel.app/timeline")
-
     val topAppBarColors = TopAppBarDefaults.smallTopAppBarColors(
         containerColor = Color.Transparent,
     )
@@ -100,7 +101,9 @@ fun TimelineScreen(
                         )
                     }
                     .fillMaxWidth()
-                    .padding(horizontal = 16.dp),
+                    .padding(horizontal = 16.dp)
+                    .clip(RoundedCornerShape(20.dp))
+                    .clickable(onClick = onNavigateToDetails),
                 painter = painterResource(
                     id = when (page) {
                         0 -> R.drawable.dummy_img_timeline_1
@@ -113,33 +116,6 @@ fun TimelineScreen(
             )
         }
     }
-
-    /* WebView(
-         modifier = modifier.weight(1f),
-         state = webViewState,
-         onCreated = { webView ->
-             with(webView) {
-                 settings.run {
-                     javaScriptEnabled = true
-                     domStorageEnabled = true
-                     javaScriptCanOpenWindowsAutomatically = false
-                     addJavascriptInterface(JsBridge(), "Native")
-                 }
-             }
-         }
-     )*//*AndroidView(
-            factory = { context ->
-                WebView(context).apply {
-                    settings.javaScriptEnabled = true
-                    setBackgroundColor(android.graphics.Color.TRANSPARENT)
-                    loadUrl("file:///android_asset/webviewbridge.html")
-                    addJavascriptInterface(
-                        JsBridge(),
-                        "Native",
-                    )
-                }
-            },
-        )*/
 }
 
 class JsBridge {
